0c7edfe22a2bc8c0080843d6cb23d0b8717a1f33
Palash Bhowmick <palash.bhowmick@gmail.com>
2016-05-23 16:51:03 +0530

    initial commit for gui, added dependency and test view

---
+++ src/main/java/com/neelhridoy/view/MainPanel.java
@@ -0,0 +1,40 @@
+package com.neelhridoy.view;
+
+import com.alee.laf.WebLookAndFeel;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * Created by Palash on 5/23/2016.
+ */
+public class MainPanel {
+    public MainPanel() {
+        //todo support for browse folder and export excel
+        JPanel panel = new JPanel();
+        JButton jButton = new JButton("Browse");
+        panel.add(jButton);
+
+        JFrame frame = new JFrame("View Test");
+        frame.add(panel);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        Dimension defaultSize = new Dimension(690, 500);
+        frame.setSize(defaultSize);
+        frame.setMinimumSize(defaultSize);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setLocation((screenSize.width / 2) - 300, (screenSize.height / 2) - 250);
+        frame.setTitle("Payslip Organizer");
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        try {
+            WebLookAndFeel.setDecorateAllWindows(true);
+            UIManager.setLookAndFeel("com.alee.laf.WebLookAndFeel");
+
+            new MainPanel();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
